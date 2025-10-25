2818c4d7cbd9b52776184ceebccce2ab89377a82
Jagadeesh378 <jagadeeshkemburu2000@gmail.com>
2025-10-25 19:02:09 +0530

    created the Room Request, ResponseDto's

---
+++ src/main/java/com/flmhospitals/requestDto/RoomRequestDto.java
@@ -0,0 +1,14 @@
+package com.flmhospitals.requestDto;
+
+import java.util.List;
+
+public class RoomRequestDto {
+
+	private long roomNumber;
+	
+	private String roomType;
+	
+	private String capacity;
+	
+	private List<Bed> beds;
+}
---
+++ src/main/java/com/flmhospitals/responseDto/RoomResponseDto.java
@@ -0,0 +1,16 @@
+package com.flmhospitals.responseDto;
+
+import java.util.List;
+
+public class RoomResponseDto {
+	
+	private long roomId;
+	
+	private long roomNumber;
+	
+	private String roomType;
+	
+	private String capacity;
+	
+	private List<Bed> beds;
+}
